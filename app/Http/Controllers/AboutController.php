4c190a91ed6c28101db4067ca731106920024736
FaiqPrasetya <2041720239@student.polinema.ac.id>
2022-02-28 22:38:55 +0700

    Single action controller added

--- app/Http/Controllers/AboutController.php
+++ app/Http/Controllers/AboutController.php
@@ -4,18 +4,10 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class PageController extends Controller {
-    public function index() {
-        echo "Selamat Datang";
-    }
-
+class AboutController extends Controller {
     public function about() {
         echo "NIM   : 2041720239 <br>";
         echo "Nama  : Muhammad Faiq Prasetya <br>";
         echo "Kelas : TI-2D <br>";
-    }
-
-    public function articles($id) {
-        echo "Ini merupakan halaman artikel dengan id " . $id;
     }
 }
